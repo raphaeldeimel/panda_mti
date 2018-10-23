da15e8dfeeb7653022b1ae24e65b130f88953a12
Unknown <alexander.pawluchin@campus.tu-berlin.de>
2018-10-23 18:06:59 +0200

    interpolation added

--- panda_friction/src/panda_friction.py
+++ panda_friction/src/panda_friction.py
@@ -11,137 +11,98 @@ import numpy as np
 import rospy
 
 from std_msgs.msg import String
-from mti_panda_controller_msgs.msg import PDControllerGoal8
-from mti_panda_controller_msgs.msg import RobotState8
-
-# global variables
-pandaRobotState = RobotState8()
-robotStatesAvailable = False
+from panda_msgs_mti.msg import PDControllerGoal8, RobotState8
 
 
-# update robot states
-def robotHandler(states):
-    robotStatesAvailable = True
-    pandaRobotState = states
+class frictionMeasurement:
+    def __init__(self, joint_d, q_d):
+        rospy.init_node('panda_friction_node', anonymous=True)
+        rospy.loginfo("init")
+        self.pandaRobotState = RobotState8()
+        self.robotStatesAvailable = False
+
+        self.phaseIndex = 0
+        self.phaseState = ['homing', 'acc', 'rec', 'dec', 'stop']
+
+        self.homingTime = 2
+        self.desired_dot_q = q_d
+        self.desired_joint = joint_d
+
+        self.jointSafetyOffset = 0.1
+
+        self.trajSize = (8, 25*self.homingTime)
+        self.interpDesiredTraj = np.zeros(self.trajSize)
+
+        self.jLimits = [
+                [-2.89 + self.jointSafetyOffset, +2.89 - self.jointSafetyOffset], # j1
+                [-1.76 + self.jointSafetyOffset, +1.76 - self.jointSafetyOffset],
+                [-2.89 + self.jointSafetyOffset, +2.89 - self.jointSafetyOffset],
+                [-3.07 + self.jointSafetyOffset, -0.06 - self.jointSafetyOffset],
+                [-2.89 + self.jointSafetyOffset, +2.89 - self.jointSafetyOffset],
+                [-0.01 + self.jointSafetyOffset, +3.75 - self.jointSafetyOffset],
+                [-2.89 + self.jointSafetyOffset, +2.89 - self.jointSafetyOffset]] # j7
+
+	    # column: joint setup for the corresponding measurement
+	    # rows: joint position (q1 - q7)
+        self.jointHomePose = [
+                [+2.89, +0.00, +0.00, +0.00, +0.00, +0.00, +0.00], # j1
+                [+0.00, +2.89, +0.00, +1.54, +0.00, +1.54, +0.00],
+                [+0.00, +1.54, +2.89, +1.54, +0.00, +1.54, +0.00],
+                [-0.10, -2.73, -0.10, -0.10, -0.10, -0.10, -0.10],
+                [+0.00, +0.00, +0.00, +0.00, +2.89, +0.00, +0.00],
+                [+3.14, +2.73, +3.14, +1.54, +3.14, +0.00, +3.14],
+                [+0.00, +0.00, +0.00, +0.00, +0.00, +0.90, +2.89]] # j7
+
+        self.jointKP = [20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0] # [j1 ... j7]
+        self.jointKV = [30.0, 30.0, 20.0, 20.0, 20.0, 10.0, 10.0] # [j1 ... j7]
+
+        self.startROS()
 
 
-
-def followTraj():
-
-	# ----- ROS -----
-	# goal publisher
-    goal_pub = rospy.Publisher('/pdcontroller_goal', PDControllerGoal8, queue_size=10)
-	# publishes the motion phases
-    state_pub = rospy.Publisher('/panda/friction/state_pub', String, queue_size=10)
-	# reads the joint robot states of the robot
-    rospy.Subscriber("/panda/currentstate", RobotState8, robotHandler)
-	#ROS main node
-    rospy.init_node('panda_friction_node', anonymous=True)
-
-	#----- INIT -----
-    # motion variables
-	# time to get home
-    homingTime = 5.0
-    # desired joint velocity
-    desired_dot_q = 0.3
-    desired_joint = 1 # 1 - 7
-
-    # Goal publisher
-    jtTraj = PDControllerGoal8()
-
-    jtTraj.torque = np.zeros(8);
-    jtTraj.position = np.zeros(8);
-    jtTraj.velocity = np.zeros(8);
-    jtTraj.kp = np.zeros(8);
-    jtTraj.kv = np.zeros(8);
-
-    phaseIndex = 0
-    phaseState = [
-        'homing',
-        'acceleration',
-        'recording',
-        'deceleration',
-        'stop']
-
-    jointSafetyOffset = 0.1
-    jLimits = [
-        [-2.89 + jointSafetyOffset, +2.89 - jointSafetyOffset], # joint1
-        [-1.76 + jointSafetyOffset, +1.76 - jointSafetyOffset],
-        [-2.89 + jointSafetyOffset, +2.89 - jointSafetyOffset],
-        [-3.07 + jointSafetyOffset, -0.06 - jointSafetyOffset],
-        [-2.89 + jointSafetyOffset, +2.89 - jointSafetyOffset],
-        [-0.01 + jointSafetyOffset, +3.75 - jointSafetyOffset],
-        [-2.89 + jointSafetyOffset, +2.89 - jointSafetyOffset]] # joint7
-
-	# nesting
-	# column: joint setup for the corresponding measurement
-	# rows: joint position (q1 - q7)
-    jointHomePose = [
-        [+2.89, +0.00, +0.00, +0.00, +0.00, +0.00, +0.00], #q1
-        [+0.00, +2.89, +0.00, +1.54, +0.00, +1.54, +0.00], #q2
-        [+0.00, +1.54, +2.89, +1.54, +0.00, +1.54, +0.00],
-        [-0.10, -2.73, -0.10, -0.10, -0.10, -0.10, -0.10],
-        [+0.00, +0.00, +0.00, +0.00, +2.89, +0.00, +0.00],
-        [+3.14, +2.73, +3.14, +1.54, +3.14, +0.00, +3.14],
-        [+0.00, +0.00, +0.00, +0.00, +0.00, +0.90, +2.89]] #q7
-
-    jointKP = [
-        20, # joint1
-        20,
-        20,
-        20,
-        20,
-        20,
-        20] # joint7
-
-    jointKD = [
-        30, # joint1
-        30,
-        20,
-        20,
-        20,
-        10,
-        10] # joint7
-
-	# ----- HOMING -----
-    time.sleep(1)
-	# exit program if joint states are not published
-    if not robotStatesAvailable:
-        print("error: no robot states available, quiting...")
-        quit()
-
-	# position interpolation
+    def robotStateHandler(self, states):
+        self.robotStatesAvailable = True
+        self.pandaRobotState = states
 
 
+    def getHomeTraj(self):
+        # interpolates joint position between actual value and desired start position
+        for i in range(7):
+            self.interpDesiredTraj[i] = np.linspace(self.pandaRobotState.q[i], self.jointHomePose[i][self.desired_joint], self.trajSize[1])
 
-    # Homing all / step trajectory
-	"""
-    for i in range(0,7):
-        if not i == desired_joint-1:
-            jtTraj.position[i] = jointStaticPose[i][desired_joint-1]
-        else:
-            jtTraj.position[i] = jLimits[i][0]
-        jtTraj.kp[i] = jointKP[i]
-    goal_pub.publish(jtTraj)
-    state_pub.publish(phaseState[0]) # homing
 
-	"""
-
-    # time.sleep(5)
-
-    rate = rospy.Rate(25) # 25 Hz
-    print("hallo")
-    rospy.loginfo("publishing")
-	# main control loop
-    while not rospy.is_shutdown():
+    def getMoveTraj(self):
+        rospy.loginfo("moving...")
         pass
-        # publish new gaol for the desired joint
-        # goal_pub.publish(jtTraj)
-        # state_pub.publish(phaseState[phaseIndex])
 
-        rate.sleep()
+
+    def startROS(self):
+        self.goal_pub = rospy.Publisher('/pdcontroller_goal', PDControllerGoal8, queue_size=10)
+        state_pub = rospy.Publisher('/panda/friction/state_pub', String, queue_size=10)
+        rospy.Subscriber("/panda/currentstate", RobotState8, self.robotStateHandler)
+        rospy.init_node('panda_friction_node', anonymous=True)
+
+        rospy.loginfo("waiting for pdcontroller...")
+        time.sleep(2)
+
+        if not self.robotStatesAvailable:
+            print("error: no robot states available, quiting...")
+            quit()
+
+        self.getHomeTraj()
+        # for loop for homing
+
+        rate = rospy.Rate(25) # 25 Hz
+
+        rospy.loginfo("recording...")
+	       # main control loop
+        while not rospy.is_shutdown():
+            print(self.pandaRobotState)
+            pass
+            rate.sleep()
 
 
 
 if __name__ == '__main__':
-    followTraj()
+    joint = 0
+    q_dot_desired = 0.1
+    frictionObj = frictionMeasurement(joint, q_dot_desired)
