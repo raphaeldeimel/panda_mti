a903232b8d9ccc7e992c03ceae8747e1c37b32e3
alex <alexander.pawluchin@campus.tu-berlin.de>
2018-11-06 09:43:04 +0100

    prints now all 7 joints

--- panda_friction/scripts/friction_torque_figure.py
+++ panda_friction/scripts/friction_torque_figure.py
@@ -25,33 +25,49 @@ _dataStack_tau_full = []
 _dataStack_dq = []
 _dataStack_tau = []
 
-currentJoint = 1
+f, axarr = plt.subplots(7, sharex=True)
+f.suptitle('friction measurement')
 
 for listIndex in range(len(bagNames)):
+    # get current bag name
     currentBag = bagNames[listIndex]
-    jtNum = int(currentBag[1])-1
-    if jtNum > 0:
-        break
+    # get the joint from current bag name
+    jtCur = int(currentBag[1])-1
+    # load bag
     bag = rb.Bag(bagPath + currentBag, 'r')
-
-    print(currentBag)
-
+    # extract bag message
     for (topic, msg, t) in bag.read_messages(topics=[topicNameState, topicNamePhase]):
+        # look for the "recording" tag
         if topic == topicNamePhase and msg.data == "recording":
             _recMode = True
 
+        # if deceleration tag is found, stop filling the data stack
         if topic == topicNamePhase and msg.data == "deceleration":
             _dataStack_dq_full.append(_dataStack_dq)
             _dataStack_tau_full.append(_dataStack_tau)
+            print(len(_dataStack_dq_full))
             _recMode = False
+            _printMode = True
             bag.close()
             break
 
+        # fill the data stack
         if topic == topicNameState and _recMode:
-            _dataStack_dq.append(-1*msg.dq[jtNum])
-            _dataStack_tau.append(-1*msg.tau[jtNum])
+            _dataStack_dq.append(-1*msg.dq[jtCur])
+            _dataStack_tau.append(-1*msg.tau[jtCur])
 
-plt.scatter(_dataStack_dq_full,_dataStack_tau_full, s=0.1)
-plt.xlabel("dq")
-plt.ylabel("tau")
+    print('bagname: ' + currentBag + '\t (%s/%s)' % (1+listIndex, len(bagNames)))
+    if _printMode:
+        axarr[jtCur].scatter(_dataStack_dq_full, _dataStack_tau_full, s=0.1, color='r')
+        axarr[jtCur].set_title('joint ' + str(jtCur+1))
+        _dataStack_dq_full = []
+        _dataStack_tau_full = []
+        _dataStack_dq = []
+        _dataStack_tau = []
+        _printMode = False
+
+# for ax in axarr.flat:
+    # ax.set(ylabel='tau [Nm]')
+plt.xlabel("joint velocity dq [rad/s]")
 plt.show()
+
