e4f813677e9d38971a4ff3796c286271c86f4337
raphael.deimel <raphael.deimel@tu-berlin.de>
2019-01-15 18:37:41 +0100

    ping panda before resolving mac address

--- controller/scripts/getHardwareID.sh
+++ controller/scripts/getHardwareID.sh
@@ -2,8 +2,15 @@
 
 dpkg -s net-tools > /dev/null 2>&1
 
+if [ -z "$1" ];then
+    NAME=panda
+else
+    NAME=$1
+fi
+
 if [ $? -eq 0 ]; then
-    arp -a $1  | awk '{print $4}' | sed s/://g | tr -d '\n'
+    ping -c 1 -l 1 -n -q $NAME >/dev/null #ensure ARP cache is populated
+    arp -a $NAME  | awk '{print $4}' | sed s/://g | tr -d '\n'
 else
     echo "dpkg-error"
 fi
