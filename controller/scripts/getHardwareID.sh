bcbb20bb25f6159e2346803f3bdbb6ac57fdbba5
alex <alexander.pawluchin@campus.tu-berlin.de>
2019-01-17 09:36:44 +0100

    dpkg handling added

--- controller/scripts/getHardwareID.sh
+++ controller/scripts/getHardwareID.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
-
 dpkg -s net-tools > /dev/null 2>&1
 
-if [ $? -eq 0 ]; then
-    arp -a $1  | awk '{print $4}' | sed s/://g | tr -d '\n'
+if [ -z "$1" ]; then
+    NAME=panda
 else
-    echo "dpkg-error"
+    NAME=$1
 fi
+
+if [ $? -eq 0 ]; then
+    ping -c 1 -l 1 -n -q $NAME >/dev/null #populated arp cache
+    MACDATA=$(arp -a panda)
+    if [ "$(echo $MACDATA | awk '{print $1}')" = "arp:" ]; then
+    	echo "No Panda found. Please connect panda to the network and start panda"
+    else
+        export PANDA_MAC="$(arp -a panda  | awk '{print $4}' | sed s/://g | tr -d '\n')"
+    fi
+else
+    echo "Error while executing net-tools!
+    
+    Please make sure that net-tools is installed.
+    try: sudo apt install net-tools"
+fi
+
