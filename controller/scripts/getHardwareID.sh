3a499c756bcca639e5c3853a9061a62820fa5d79
Roman Kolbert <roman.kolbert@tu-berlin.de>
2019-03-04 12:33:16 +0100

    New sticktion parameters

--- controller/scripts/getHardwareID.sh
+++ controller/scripts/getHardwareID.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
+
 dpkg -s net-tools > /dev/null 2>&1
 
-if [ -z "$1" ]; then
+if [ -z "$1" ];then
     NAME=panda
 else
     NAME=$1
 fi
 
 if [ $? -eq 0 ]; then
-    ping -c 1 -l 1 -n -q $NAME >/dev/null #populated arp cache
-    MACDATA=$(arp -a panda)
-    if [ "$(echo $MACDATA | awk '{print $1}')" = "arp:" ]; then
-    	echo "No Panda found. Please connect panda to the network and start panda"
-    else
-        export PANDA_MAC="$(arp -a panda  | awk '{print $4}' | sed s/://g | tr -d '\n')"
-    fi
+    ping -c 1 -l 1 -n -q $NAME >/dev/null #ensure ARP cache is populated
+    arp -a $NAME  | awk '{print $4}' | sed s/://g | tr -d '\n'
 else
-    echo "Error while executing net-tools!
-    
-    Please make sure that net-tools is installed.
-    try: sudo apt install net-tools"
+    echo "dpkg-error"
 fi
 
