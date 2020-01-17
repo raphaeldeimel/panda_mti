d69189155af158be10fc1e92964d8aa8224f84fc
Roman <roman.kolbert@tu-berlin.de>
2020-01-17 13:59:28 +0100

    Merge branch 'task_space_roman' into staging

--- controller/scripts/getHardwareID.sh
+++ controller/scripts/getHardwareID.sh
@@ -9,8 +9,18 @@ else
 fi
 
 if [ $? -eq 0 ]; then
-    ping -c 1 -l 1 -n -q $NAME >/dev/null #ensure ARP cache is populated
-    arp -a $NAME  | awk '{print $4}' | sed s/://g | tr -d '\n'
+    ping -c 1 -l 1 -n -w 2 -q $NAME >/dev/null #populated arp cache
+    MACDATA=$(arp -a panda)
+    if [ "$(echo $MACDATA | awk '{print $1}')" = "arp:" ]; then
+    	echo "No Panda found. Please connect panda to the network and start panda" >&2
+    else
+        PANDA_MAC="$(arp -a panda  | awk '{print $4}' | sed s/://g | tr -d '\n')"
+        export PANDA_MAC #in case we got source'd
+        echo "$PANDA_MAC"
+    fi
 else
-    echo "dpkg-error"
+    echo "Error while executing net-tools!
+    
+    Please make sure that net-tools is installed.
+    try: sudo apt install net-tools"  >&2
 fi
